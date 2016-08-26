b597698974dc5b3ba8beb5befc874f5d9d1caf19
Brett New <brett.new91@gmail.com>
2016-08-26 14:11:52 -0700

    allow reviews to be saved to specific books.

--- app/routes/book.js
+++ app/routes/book.js
@@ -3,5 +3,27 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params){
     return this.store.findRecord('book', params.book_id);
+  },
+  actions: {
+    saveReview(params) {
+      if (params.rating === '*'){
+        params.rating = 1;
+      } else if (params.rating ==='**') {
+        params.rating = 2;
+      } else if (params.rating === '***') {
+        params.rating = 3;
+      } else if (params.rating === '****') {
+        params.rating = 4;
+      } else {
+        params.rating = 5;
+      }
+      var newReview = this.store.createRecord('review', params);
+      var book = params.book;
+      book.get('reviews').addObject(newReview);
+      newReview.save().then(function(){
+        return book.save();
+      });
+      this.transitionTo('book', params.book);
+    }
   }
 });
