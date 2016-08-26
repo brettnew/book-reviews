136c613fa32c80d90df172b9213cb76939bbd5b7
Brett New <brett.new91@gmail.com>
2016-08-26 14:53:10 -0700

    add computed property in order to be able to display the average of all ratings for a book.

--- app/components/books-tile.js
+++ app/components/books-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  ratingAverage: Ember.computed('reviews.@each.rating', function(){
+    var total = 0;
+    (this.get('reviews')).forEach(function(review){
+      total += review.get('rating');
+    });
+    return Math.round(total/this.get('reviews').get('length'));
+  })
 });
